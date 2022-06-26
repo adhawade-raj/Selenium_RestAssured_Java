7ea163185a4096d7464a1b6233fd30aaa99d923f
adhawade-raj <adhawaderaj@gmail.com>
2022-06-26 10:46:53 +0530

    new java and selenium programs added

---
+++ Raj/src/InterviewPrograms/AditionOfArray.java
@@ -0,0 +1,21 @@
+package InterviewPrograms;
+
+public class AditionOfArray {
+
+	public static void main(String[] args) {
+
+		int a[]= {1,2,3,4,5};
+		
+		int sum = 0;
+		for(int i=0; i<a.length; i++)
+		{
+			sum = sum+a[i];
+			
+			
+		}
+		System.out.print(" Sum is " +sum);
+		
+		
+	}
+
+}
--- Raj/src/InterviewPrograms/Reverse_Array.java
+++ Raj/src/InterviewPrograms/Reverse_Array.java
@@ -7,7 +7,8 @@ public class Reverse_Array {
 		int a[]= {2,4,3,1};
 		int b=a.length-1;
 		
-		for(int i=0; i<=b; i++)
+//		for(int i=0; i<b; i++)
+		for(int i=b; i>=0;i--)
 		{
 			System.out.print(a[i]+" ");
 		}
