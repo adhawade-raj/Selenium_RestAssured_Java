f1bc439f6589ccef7405cdd7f62d76f896837078
adhawade-raj <adhawaderaj@gmail.com>
2024-05-10 22:14:56 +0530

    RestAssured Codes Added

---
+++ 2024-RestAssured_Naveen/src/test/java/RA_05_com/product/api/E2_Products.java
@@ -0,0 +1,125 @@
+package RA_05_com.product.api;
+
+public class E2_Products {
+
+	
+	private int id;
+	private String title;
+	private float price;
+	private String description;
+	private String category;
+	private String image;
+	private Rating rating;
+	
+	public E2_Products() {}
+	
+	public E2_Products(int id, String title, float price, String description, String category, String image,
+			Rating rating) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.price = price;
+		this.description = description;
+		this.category = category;
+		this.image = image;
+		this.rating = rating;
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Rating getRating() {
+		return rating;
+	}
+
+	public void setRating(Rating rating) {
+		this.rating = rating;
+	}
+
+	
+	
+	
+	public class Rating{
+		private double rate;
+		private int count;
+		
+		public Rating() {}
+		
+		public Rating(double rate, int count) {
+			super();
+			this.rate = rate;
+			this.count = count;
+		}
+		public double getRate() {
+			return rate;
+		}
+		public void setRate(double rate) {
+			this.rate = rate;
+		}
+		public int getCount() {
+			return count;
+		}
+		public void setCount(int count) {
+			this.count = count;
+		}
+	}
+	
+}
+
+/**
+*{
+        "id": 1,
+        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        "price": 109.95,
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        "rating": {
+            "rate": 3.9,
+            "count": 120
+        }
+**/
