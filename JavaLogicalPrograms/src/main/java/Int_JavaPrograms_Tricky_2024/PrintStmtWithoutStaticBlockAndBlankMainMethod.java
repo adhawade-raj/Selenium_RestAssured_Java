e9d1e69b24777f929ebb64bcd5eb64ccb297bcfe
adhawade-raj <adhawaderaj@gmail.com>
2024-09-14 19:53:00 +0530

    Project Modifications

---
+++ JavaLogicalPrograms/src/main/java/Int_JavaPrograms_Tricky_2024/PrintStmtWithoutStaticBlockAndBlankMainMethod.java
@@ -0,0 +1,17 @@
+package Int_JavaPrograms_Tricky_2024;
+
+public class PrintStmtWithoutStaticBlockAndBlankMainMethod {
+
+	
+	public static int printString() {
+		System.out.println("Hello World");
+		return 1;
+	}
+	static int p = printString();
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
