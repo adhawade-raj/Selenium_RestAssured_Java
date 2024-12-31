e319af924e1819f692015c4d01250c9d15db8785
adhawade-raj <adhawaderaj@gmail.com>
2024-12-31 19:16:12 +0530

    Last Node removal

--- JavaLogicalPrograms/src/main/java/dsa_ScottBarret_2025/Section4_LL01_Utilities.java
+++ JavaLogicalPrograms/src/main/java/dsa_ScottBarret_2025/Section4_LL01_Utilities.java
@@ -61,6 +61,27 @@ public class Section4_LL01_Utilities {
 			tail = newNode;
 		}
 		length++;
+	}
+
+	public Node removeLst() {
+		if (length == 0)
+			return null;
+
+		Node temp = head;
+		Node pre = head;
+		while (temp.next != null) {
+			pre = temp;
+			temp = temp.next;
+		}
+		tail = pre;
+		tail.next = null;
+		length--;
+		if (length == 0) {
+			head = null;
+			tail = null;
+		}
+		System.out.println("Tail Node : " + temp.value);
+		return temp;
 
 	}
 }
--- JavaLogicalPrograms/src/main/java/dsa_ScottBarret_2025/Section4_LL03_RemoveLastNode.java
+++ JavaLogicalPrograms/src/main/java/dsa_ScottBarret_2025/Section4_LL03_RemoveLastNode.java
@@ -1,15 +1,18 @@
 package dsa_ScottBarret_2025;
 
-public class Section4_LL02_Constructor {
+public class Section4_LL03_RemoveLastNode {
 
 	public static void main(String[] args) {
-
 		Section4_LL01_Utilities ll = new Section4_LL01_Utilities(5);
 		ll.append(6);
 		ll.getHead();
 		ll.getTail();
 		ll.getLength();
 		ll.printList();
+		System.out.println("-----Removal Of Node----");
+		ll.removeLst();
+		ll.getLength();
+
 	}
 
 }
