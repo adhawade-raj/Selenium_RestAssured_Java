54c725ac839d2358817f78d96ab498677ed75297
adhawade-raj <adhawaderaj@gmail.com>
2026-08-16 13:31:52 +0530

    added new utilities

--- selenium-2026/src/main/java/int_allSeleniumCodes_2026/Utils_IntSeries.java
+++ selenium-2026/src/main/java/int_allSeleniumCodes_2026/Utils_IntSeries.java
@@ -1,5 +1,211 @@
 package int_allSeleniumCodes_2026;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class Utils_IntSeries {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Utils_IntSeries.class);
+
+    public static JavascriptExecutor js;
+
+    @Getter
+    @Setter
+    protected static WebDriver webDriver;
+    private static final long DRIVER_WAIT_TIME = 60;
+    private static final long ELEMENT_WAIT_TIME = 20;
+    String HOME_PAGE_URL = "https://www.ixigo.com/flights";
+
+    @Getter
+    protected WebDriverWait wait;
+    public final String platform = System.getenv("platform");
+    public final String browser = System.getProperty("browser");
+
+    public Utils_IntSeries() {
+
+        webDriver = new ChromeDriver();
+        this.wait = new WebDriverWait(webDriver, Duration.ofSeconds(60));
+        js = (JavascriptExecutor) webDriver;
+        PageFactory.initElements(webDriver, this);
+    }
+
+    @SneakyThrows
+    public void openHomePage() {
+        LOG.info("Web site url:" + HOME_PAGE_URL);
+        webDriver.manage().window().maximize();
+        webDriver.navigate().to(HOME_PAGE_URL);
+        LOG.info(HOME_PAGE_URL);
+    }
+
+    public void clickByElement(final By by) {
+
+        LOG.info("Running: clickByElement");
+        if (isElementPresentBy(by)) {
+            webDriver.findElement(by).click();
+        } else {
+            try {
+                LOG.info("could not find element on 1st attempt, trying again");
+                waitForExpectedElement(by).click();
+            } catch (final Exception e) {
+                LOG.info("****ERROR FINDING ELEMENT " + by);
+                LOG.info("failed to find element after 2 attempts");
+            }
+        }
+    }
+
+    public void clickUsingJS(final By by) {
+        final WebElement element = webDriver.findElement(by);
+        js.executeScript("arguments[0].click();", element);
+    }
+
+    public void scrollToViewElement(final By by) {
+        final WebElement element = webDriver.findElement(by);
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public void scrollToElementAndClick(final By by) {
+        final Actions actions = new Actions(webDriver);
+        actions.moveToElement(webDriver.findElement(by))
+                .click()
+                .build()
+                .perform();
+    }
+
+    public void scrollToElementAndClick(final WebElement element) {
+        final Actions actions = new Actions(webDriver);
+        actions.moveToElement(element)
+                .click()
+                .build()
+                .perform();
+    }
+
+    public boolean isElementPresentBy(final By by) {
+        LOG.info("Running: isElementPresentBy");
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(DRIVER_WAIT_TIME));
+        final boolean exists = !webDriver.findElements(by).isEmpty();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(DRIVER_WAIT_TIME));
+        LOG.info("Element " + by + " exists: " + exists);
+        return exists;
+    }
+
+    public boolean isDisplayedBy(final By by) {
+        LOG.info("Running: isDisplayedBy");
+        return webDriver.findElement(by).isDisplayed();
+    }
+
+    protected WebElement waitForExpectedElement(final By by) {
+        LOG.info("Running: waitForExpectedElement");
+        try {
+            return this.wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        } catch (Exception e) {
+            LOG.info("Element not found: " + by, e);
+            return null;
+        }
+    }
+
+    protected void waitForExpectedElementToBeClickable(final By by) {
+        LOG.info("Running: waitForExpectedElement");
+        this.wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
+
+    public void waitClearAndEnterText(final By by, final String textToEnter) {
+        LOG.info("Running: waitClearAndEnterText");
+        waitForExpectedElement(by).clear();
+        waitForExpectedElement(by).sendKeys(textToEnter);
+    }
+
+    public void verifyPageTitle(final String expectedTitle) {
+        Assert.assertEquals(expectedTitle, webDriver.getTitle());
+    }
+
+    public void clickUsingJSWithWebElement(final WebElement element) {
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+        js.executeScript("arguments[0].click();", element);
+    }
+
+    public void waitForSpecificElement(final By by) {
+        FluentWait<WebDriver> fluentWait = new FluentWait<>(webDriver)
+                .withTimeout(Duration.ofSeconds(300))
+                .pollingEvery(Duration.ofSeconds(5))
+                .ignoring(Exception.class);
+        fluentWait.until(webDriver -> webDriver.findElement(by).isDisplayed());
+    }
+
+    public boolean isElementClickableBy(final By by) {
+        LOG.info("Running: isElementClickable");
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(by));
+            return true;
+        } catch (Exception e) {
+            LOG.info("Element is not clickable: " + by);
+            return false;
+        }
+    }
+
+    public void hoverOnElement(final By by) {
+        final Actions actions = new Actions(webDriver);
+        actions.moveToElement(webDriver.findElement(by)).build().perform();
+    }
+
+    public void hoverOnElementAndClickSubmenu(final By mainMenuBy, final By by) {
+        scrollToTopOfPage();
+        hoverOnElement(mainMenuBy);
+        waitForExpectedElement(by).click();
+    }
+
+    public void waitForJavaScriptLoad() {
+        new WebDriverWait(webDriver, Duration.ofSeconds(30))
+                .until((ExpectedCondition<Boolean>) wd ->
+                        ((JavascriptExecutor) wd)
+                                .executeScript("return document.readyState")
+                                .equals("complete"));
+    }
+
+    public void scrollToEndOfPage() {
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) webDriver;
+        jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void scrollToTopOfPage() {
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) webDriver;
+        jsExecutor.executeScript("window.scrollTo(0, 0);");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isItAvailableBy(final By by) {
+        LOG.info("Running: isItAvailableBy");
+        try {
+            return webDriver.findElement(by).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
 }
